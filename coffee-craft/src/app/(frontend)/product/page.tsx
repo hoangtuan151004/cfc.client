a84594e695ee488e52606fd87e52d687c7b1f5d7
hoangtuan151004 <tuanvh.dev@gmail.com>
2025-05-08 17:38:22 +0700

    fix deploy

--- coffee-craft/src/app/(frontend)/product/page.tsx
+++ coffee-craft/src/app/(frontend)/product/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import NewsLetter from "../../../components/Home/NewsLetter";
 import Banner from "../../../components/AllProduct/Banner";
 import ShowPro from "../../../components/AllProduct/Show-pro";
@@ -8,12 +8,14 @@ import { CardContent } from "../../../components/ui/card";
 const AllProducts: React.FC = () => {
   return (
     <>
-      <Banner />
-      <section className="container min-h-screen lg:px-16 md:px-8 px-4 py-10">
-        <ProductFilter />
-        <ShowPro />
-        <NewsLetter />
-      </section>
+      <Suspense fallback={<div>Đang tải sản phẩm...</div>}>
+        <Banner />
+        <section className="container min-h-screen lg:px-16 md:px-8 px-4 py-10">
+          <ProductFilter />
+          <ShowPro />
+          <NewsLetter />
+        </section>
+      </Suspense>
     </>
   );
 };
